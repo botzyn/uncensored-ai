2f28740974f272ab69ed5bedd5eabfe012bff44e
Botzyn <botzyn@protonmail.com>
2026-02-18 03:01:02 -0800

    fix: Configure Vercel static build + API routes

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,5 @@
 // Vercel API Route for Chat
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     const { message, history = [] } = req.body;
     
     if (!message) {
